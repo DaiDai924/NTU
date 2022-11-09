f2592847016626c778915d7d015ee17e967724f4
DaiDai924 <55429163+DaiDai924@users.noreply.github.com>
2022-11-10 01:46:46 +0800

    hw1 finish

---
+++ ADL/HW1/download.sh
@@ -0,0 +1,4 @@
+gdown --fuzzy https://drive.google.com/file/d/1BzZ2MJB14O6ZQjwjB7AdUIOI1HPN-uG3/view?usp=share_link
+
+# wget https://www.dropbox.com/s/l00v67sz94olfm1/ADL-Hw1_bestmodel.zip?dl=1 -O bestmodel.zip
+# unzip bestmodel.zip
---
+++ ADL/HW1/slot_tag.sh
@@ -0,0 +1,2 @@
+unzip bestmodel.zip
+python test_slot.py --test_file "${1}" --pred_file "${2}"
